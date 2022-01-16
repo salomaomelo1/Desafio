36a7baac240b733fa50ed46b5a1ac0b5b53b4a2a
Salomao Albuquerque <salomaomelo1@gmail.com>
2022-01-16 13:26:12 -0300

    atualizar programa

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#staus');
 
-
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
 pegarPersonagem = () => {
-    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+    let numeroAleatorio =gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, 
+    {
         method: 'GET',
         headers:{
             Accept: 'application/json',
